e7b29705c66d00e1e744119a76d275a03724bcc7
RobertVJ <43622574+RobertVJ@users.noreply.github.com>
2022-09-08 17:00:05 -0500

    parte 2 del  preprosesing
    Se agregaron los graficos base faltantes , como tambien el despligue del codigo

--- processing/df_generator.py
+++ processing/df_generator.py
@@ -35,12 +35,18 @@ import pylab
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from  sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import random
 
 
 # /-----------------------------------------------------------------/
+"""
+TODO:
+re insertar anomalias
+comentarle a regi de la normalizacion de los dato  para la graficacion de los mismos
+generar funcion que corra como "menu" el codigo para su llamada prueba final (solo para pruebas)
 
+"""
 
 class DataframeGenerator:
     # Initializer of the class
@@ -75,16 +81,21 @@ class DataframeGenerator:
     # Checks the amount of rows  of the dataframe
     def check_data(self, amount):
         return  self.df_original_spaceTitanic.head(amount)
-    def check_data_porcentage(self):
+
+    # Gives  the percentage of the missing values
+    def check_data_Percentage(self):
         return self.df_original_spaceTitanic.isnull().sum() * 100/self.df_original_spaceTitanic.shape[0]
+    # Give us the  descriptive staditics  and summarizes.
     def data_decribe(self):
         return  self.df_original_spaceTitanic.describe()
+    # Checks the dataframe of duplicated values.
     def data_duplicated(self):
         return  self.df_original_spaceTitanic.duplicated().sum()
 
     # -------------------------------------------------------------------#
     # Standard Function for the Label  of the dataframe
 
+    # Auxiliary function 1
     # Rellenado de valores por frecuencia de  los mismos
     def missing_fill(self, df):
         missing_feature_freq = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP']
@@ -93,26 +104,33 @@ class DataframeGenerator:
             df[feature] = df[feature].fillna(most_freq)
         return df
 
-    # Extraccion de los dato de la columna  cabin
-    # Extraccion de la cabina inicial
+
+    # Extraction functions
+
+    # Auxiliary function 2
+    # Extraction of the main deck of the cabin
     def extractMainDeck(self, s):
         return s.split('/')[0]
 
-    # Extraccion de la del numero de cabina
+    # Auxiliary function 3
+    # Extraction of the cabin's number
     def extractNumber(self, s):
         return s.split('/')[1]
 
-    # Extraccion de del area de la cabina (lado de la nave espacial (Ventana o pasillo))
+    # # Auxiliary function 4
+    # Extraction of the cabin's area (Window or hallway )
     def extractSide(self, s):
         return s.split('/')[2]
 
-    # Tumbar columnas no Necesarias
+    # Auxiliary function 5
+    # Dropping the unnecessary columns
     def drop_columns(self, df):
-        drop_column = ['PassengerId', 'Cabin', 'Name']  # Duda con el PassengerId
+        drop_column = ['PassengerId', 'Cabin', 'Name']  # Duda con el PassengerId y name
         for ft in drop_column:
             df = df.drop(ft, axis=1)
         return df
 
+    # Auxiliary function 6
     # LabelEncoder
     def encoder(self, df):
         # Columnas para el labeLEncoder
@@ -132,25 +150,38 @@ class DataframeGenerator:
         df = pd.get_dummies(df)
         return df  # y regresamos el dataframe
 
+    # Main label-categorial fuction
     def categoricalPreprossesing(self):
-        # Llamada de las funciones
+        # Main Dataframe/Dataset
         dataset = self.df_original_spaceTitanic
-
+        # Calls of the auxiliary functions
         df = self.missing_fill(dataset)
-        # Llamada de la funcion de los datos de la columna cabina
+        # Dividing the cabin information in different sections
         df['Deck'] = df['Cabin'].apply(self.extractMainDeck)
         df['Num'] = df['Cabin'].apply(self.extractNumber)
         df['Side'] = df['Cabin'].apply(self.extractSide)
-        df = self.drop_columns(df)  # Tumbamos las columnas
+        df = self.drop_columns(df)  # Drop unnecessary columns
         df = self.encoder(df)  # Label Encoder
+        self.df_original_spaceTitanic = df.copy() # copy all the process to the main dataframe
+        return  self.df_original_spaceTitanic # returns the dataframe  with the categorical modifations
 
-        return df  # regresamos el df ya finalmente modificado
+    # Main Trasportation encoder
+    def trasportartedPreprossesing(self):
+        label_encoder = LabelEncoder()
+        # Main Dataframe/Dataset
+        dataset = self.df_original_spaceTitanic.copy()
+        transported = dataset['Transported']
+        transported_encoder = label_encoder.fit_transform(transported)
+        transported_DF = pd.DataFrame(transported_encoder, columns=['Transported'])  # dataframe
+        self.df_original_spaceTitanic.drop(columns=['Transported'], inplace=True)
+        df_new_trasform_space_titanic= pd.concat([self.df_original_spaceTitanic,transported_DF], axis = 1, verify_integrity=True)
+        self.df_original_spaceTitanic = df_new_trasform_space_titanic.copy()  # copy all the process to the main dataframe
+        return   self.df_original_spaceTitanic
 
- 
     # -------------------------------------------------------------------#
     # Standard Function for the numeric  and imputation of the dataframe
     # -------------------------------------------------------------------#
-    def imputateNumeric(self):
+    def imputatePreprossesing(self):
         # Media Room Service
         room_service_mean = self.df_original_spaceTitanic['RoomService'].mean()
         self.df_original_spaceTitanic['RoomService'] = self.df_original_spaceTitanic['RoomService'].fillna(room_service_mean)
@@ -177,7 +208,43 @@ class DataframeGenerator:
         self.df_original_spaceTitanic['VRDeck'] = self.df_original_spaceTitanic['VRDeck'].fillna(vrdeck_mean)
 
     # -------------------------------------------------------------------#
+    # Standard  deployment Function of the dataframe
+    # -------------------------------------------------------------------#
 
+    def deploymentSets(self, size = 0.2, random = 0):
+        X_train, X_val, Y_train, Y_val = train_test_split(self.df_original_spaceTitanic.drop(['Transported'], axis=1),  # X Values
+                                                          self.df_original_spaceTitanic['Transported'], test_size=size, # Y Values
+                                                          random_state=random)  # random seed to get same/
+        # seed de random para tener los mismos resultados
+        return X_train, X_val, Y_train, Y_val
+
+    # -------------------------------------------------------------------#
+    # Standard  data Visualization of the dataframe
+    # -------------------------------------------------------------------#
+
+    def heatMap(self):
+        ax = sns.heatmap(
+            self.df_original_spaceTitanic.corr(),
+            vmin=-1, vmax=1, center=0,
+            cmap=sns.diverging_palette(20, 220, n=200),
+            square=True
+        )
+        ax.set_xticklabels(
+            ax.get_xticklabels(),
+            rotation=45,
+            horizontalalignment='right'
+        )
+
+    def Histograms(self):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        self.df_original_spaceTitanic.hist(ax=ax)
+        plt.show()  # presentamos la graficacion
+
+    def Boxplot(self):
+        fig, ax = plt.subplots(figsize=(10, 10))
+        self.df_original_spaceTitanic.boxplot(ax=ax)
+        plt.xticks(rotation=90)
+        plt.show()
 
 # Call sample
 address1 = r'C:\Users\rober\OneDrive\Escritorio\Repositorios Git\AI_Repo_Team8\pre-data\Titanic\train.csv' # First csv(training)
@@ -197,7 +264,7 @@ print('/------------------------------------/')
 print(prueba.check_data(10))
 print('/------------------------------------/')
 # check_data_porcentage
-print(prueba.check_data_porcentage())
+print(prueba.check_data_Percentage())
 print('/------------------------------------/')
 # data_decribe
 print(prueba.data_decribe())
@@ -207,11 +274,23 @@ print(prueba.data_duplicated())
 print('/------------------------------------/')
 # Standard Label Encoder format
 prueba.categoricalPreprossesing()
-
+prueba.trasportartedPreprossesing()
 print('/------------------------------------/')
 print('/------------------------------------/')
 # Standard Numeric Imputation format
-prueba.imputateNumeric()
+prueba.imputatePreprossesing()
 print(prueba.null()) # We check that has  zeros missing values
                      # and we see that there is no missing values
+print('/------------------------------------/')
+# Standard Numeric Imputation format
+prueba.imputatePreprossesing()
+print('/------------------------------------/')
+size = 0.3
+random = 200
+X_train, X_val, Y_train, Y_val = prueba.deploymentSets(size,random)
+print('Train Datasets')
+print(f'Xtrain: \n{X_train}')
+print(f'X_val: \n{X_val}')
+print(f'Y_train: \n{Y_train}')
+print(f'Y_val: \n{Y_val}')
 print('/------------------------------------/')
