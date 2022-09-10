c8a980853d52577ecc1d710ef6cd8275787ce5e5
Youthan Irigoyen <a01750476@itesm.mx>
2022-09-10 00:25:15 -0500

    Minor changes to df_generator

--- processing/data_cleaning_tester.py
+++ processing/data_cleaning_tester.py
@@ -1,4 +1,17 @@
 import os
-import sys
+import numpy as np
+import pandas as pd
+import df_generator as dfg
 
-data_folder = os.path.join(os.path.abspath(os.path.curdir), 'data')
+data_file = os.path.join(os.path.abspath(os.path.curdir), 'data', 'train.csv')
+
+df_gen = dfg.DataframeGenerator(data_file)
+df = df_gen.df
+
+def compute_smd(data_1, data_2):
+    data_1 = np.array(data_1)
+    data_2 = np.array(data_2)
+    return np.abs((data_1.mean() - data_2.mean())) / np.sqrt((np.power(data_1.std(), 2) + np.power(data_2.std(), 2)) / 2)
+
+
+print(compute_smd(pd.DataFrame([1, 2, 3, 4, 5]), pd.DataFrame([2, 3, 4, 5, 6])))
