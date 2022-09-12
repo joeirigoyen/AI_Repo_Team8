c0f109a52e0ed3939f1bcc9324486ba8c8d319ad
Youthan Irigoyen <a01750476@itesm.mx>
2022-09-12 01:01:49 -0500

    Fixed details in sample processing

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, jsonify
 import csv
+from flask import Flask, request, jsonify
+
 
 from processing.df_generator import DataHandler
 
@@ -10,15 +11,22 @@ server = Flask(__name__)
 @server.route('/form', methods=['POST'])
 def input_data():
     passengers = request.json["Passengers"]
-    df_header = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'Age', 'VIP', 'RoomService', 'FoodCourt', 'Shopping', 'Spa', 'VRDeck', 'Name']
-    path = os.path.join("tmp", "test.csv")
+    df_header = ['PassengerId', 'HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'Age', 'VIP', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck', 'Name']
+    dir_path = os.path.join(os.path.abspath(os.path.curdir), "tmp")
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    path = os.path.join(dir_path, "test.csv")
     with open(path, 'w', encoding='UTF8', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(df_header)
-                for i in range(len(passengers)):
-                    input_data = passengers[i].values()
-                    writer.writerow(input_data)
-    dh = DataHandler(path)
+        writer = csv.DictWriter(file, fieldnames=df_header)
+        writer.writeheader()
+        for passenger in passengers:
+            writer.writerow(passenger)
+
+    train_path= os.path.join(os.path.abspath(os.path.curdir), "data", "train.csv")
+    test_path= os.path.join(os.path.abspath(os.path.curdir), "tmp", "test.csv")
+    dh = DataHandler(train_path)
+    sample = dh.process_data(test_path, 0, sample=True)
+    print(sample.head())
     return jsonify({"message": "Success"}), 200
 
 if __name__ == '__main__':
