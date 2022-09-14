a3025ab14597ff5b51cae28bfcf5077679b19614
Youthan Irigoyen <a01750476@itesm.mx>
2022-09-13 23:32:05 -0500

    Altered directories

---
+++ processing/random_forest.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+class RFClassifier:
+    def __init__(self, x, y, trees, quality_criterion, depth, min_split, min_leaf, features):
+        self.x = x
+        self.y = y
+        self.classifier = RandomForestClassifier(n_estimators=trees, criterion=quality_criterion, max_depth=depth, min_samples_split=min_split, min_samples_leaf=min_leaf, max_features=features, n_jobs=-1)
+
+    def adjust(self):
+        self.classifier.fit(self.x, self.y)
+
+    def predict(self, sample):
+        return self.classifier.predict(sample)
